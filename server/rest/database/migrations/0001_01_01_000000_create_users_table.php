6330d550d8339c4e0b05aab09887d2cc70563a5e
ishaan khurana <ishaankhurana123@gmail.com>
2024-04-15 14:31:59 +0530

    feat: alter the user's table

--- server/rest/database/migrations/0001_01_01_000000_create_users_table.php
+++ server/rest/database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,9 +15,9 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->integer("age");
+            $table->string("sex"); // i'd use an enum usually
             $table->timestamps();
         });
     }
