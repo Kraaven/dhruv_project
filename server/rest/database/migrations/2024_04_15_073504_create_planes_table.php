c403b88c904caef36ca8771ed09ee79c7c826614
ishaan khurana <ishaankhurana123@gmail.com>
2024-04-15 13:40:27 +0530

    feat: init models and migrations

--- server/rest/database/migrations/2024_04_15_073504_create_planes_table.php
+++ server/rest/database/migrations/2024_04_15_073504_create_planes_table.php
@@ -11,13 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('planes', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string("name");
+            $table->integer("capacity");
+            $table->unsignedBigInteger("airline_id");
+            $table->foreign("airline_id")->references("id")->on("airlines")->onDelete("cascade");
             $table->timestamps();
         });
     }
@@ -27,8 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
-        Schema::dropIfExists('password_reset_tokens');
-        Schema::dropIfExists('sessions');
+        Schema::dropIfExists('planes');
     }
 };
--- server/rest/database/migrations/2024_04_15_075601_create_tickets_table.php
+++ server/rest/database/migrations/2024_04_15_075601_create_tickets_table.php
@@ -11,13 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('tickets', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->json("seat");
+            $table->unsignedBigInteger("user_id");
+            $table->unsignedBigInteger("flight_id");
+            $table->foreign("user_id")->references("id")->on("users")->onDelete("cascade");
             $table->timestamps();
         });
     }
@@ -27,8 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
-        Schema::dropIfExists('password_reset_tokens');
-        Schema::dropIfExists('sessions');
+        Schema::dropIfExists('tickets');
     }
 };
